bf1f12c237123add6645d073bca73b4f578122e7
vishharithsa <vishresh@gmail.com>
2019-07-02 11:11:40 -0400

    2 files modified, 2 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -19,6 +19,10 @@ export class CartService {
     this.items = [];
     return this.items;
   }
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
   
   constructor(
     private http: HttpClient
